bea6b4072a7a763bbe186e2d31af586750ca82df
Anastasia <dzheyn.anna@mail.ru>
2024-10-31 21:04:23 +0400

    feat: добавляет обновление статуса и удаление задач

--- todo-list/src/components/todo-item/todo-item.tsx
+++ todo-list/src/components/todo-item/todo-item.tsx
@@ -1,27 +1,50 @@
-import { ITodo } from "@/types/todo.interface"
+import { ITodo } from '@/types/todo.interface';
 import sprite from '/sprite.svg';
+import { useAppDispatch } from '@/utils/hooks';
+import { removeTodo, updateTodo } from '@/store/todo-data/todo-data';
 
 type TodoItemProps = {
-  todo: ITodo
-}
-function TodoItem({todo}:TodoItemProps):JSX.Element {
-const {text} = todo;
-    return (
-      <div className="todo-item__wrapper">
-        <div className="todo-item__text">
+  todo: ITodo;
+};
+function TodoItem({ todo }: TodoItemProps): JSX.Element {
+  const dispatch = useAppDispatch();
+  const { id,text, completed, deleted } = todo;
+
+  const handleCompletedClick = () =>
+    dispatch(updateTodo({ ...todo, completed: !completed }));
+  const handleAddToTrashClick = () =>
+    dispatch(updateTodo({ ...todo, deleted: true }));
+  const handleDeletedClick = () =>
+    dispatch(removeTodo(id));
+
+  return (
+    <div
+      className={`todo-item__wrapper ${completed ? 'completed-todo' : ''} ${
+        deleted ? 'deleted-todo' : ''
+      }`}
+    >
+      <div className='todo-item__text'>
         <span>{text}</span>
-        </div>
-        <div className="todo-item__controls">
-        <svg className='icon' onClick={()=>{}}>
+      </div>
+      <div className='todo-item__controls'>
+        {!completed && (
+          <svg className='icon' onClick={handleCompletedClick}>
             <use xlinkHref={`${sprite}#check`}></use>
           </svg>
-        <svg className='icon' onClick={()=>{}}>
+        )}
+        {!deleted && (
+          <svg className='icon' onClick={handleAddToTrashClick}>
             <use xlinkHref={`${sprite}#trash`}></use>
           </svg>
-        </div>
+        )}
+        {deleted && (
+          <svg className='icon' onClick={handleDeletedClick}>
+            <use xlinkHref={`${sprite}#x`}></use>
+          </svg>
+        )}
       </div>
-    )
-  }
-  
-  export default TodoItem
-  
+    </div>
+  );
+}
+
+export default TodoItem;
--- todo-list/src/store/todo-data/todo-data.ts
+++ todo-list/src/store/todo-data/todo-data.ts
@@ -17,6 +17,26 @@ export const todoData = createSlice({
       saveTodos(state.todos);
     },
 
+    updateTodo: (state, { payload }: PayloadAction<ITodo>) => {
+      if (!state.todos) {
+        return;
+      }
+      const todoId = state.todos.findIndex((item) => item.id === payload.id);
+      if (todoId !== -1) {
+        state.todos[todoId] = { ...payload };
+      }
+      saveTodos(state.todos);
+    },
+
+    removeTodo: (state, { payload }: PayloadAction<number>) => {
+      if (!state.todos) {
+        return;
+      }
+      const todos = state.todos.filter((item) => item.id !== payload);
+      state.todos = todos.length > 0 ? todos : null;
+      saveTodos(state.todos);
+    },
+
     fetchTodoList: (state) => {
       state.todos = getTodos();
     },
@@ -28,4 +48,5 @@ export const todoData = createSlice({
   },
 });
 
-export const { addNewTodo, fetchTodoList, clearAllTodos } = todoData.actions;
+export const { addNewTodo, updateTodo,removeTodo, fetchTodoList, clearAllTodos } =
+  todoData.actions;
